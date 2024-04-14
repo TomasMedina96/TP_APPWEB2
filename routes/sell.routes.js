78f7f41efa5bade2e43f5cd9387be24d83c6f894
Tomas Medina <tomasmedina962@gmail.com>
2024-04-14 01:06:08 -0600

    OK

--- routes/sell.routes.js
+++ routes/sell.routes.js
@@ -1,7 +1,7 @@
 //funcion de express para crear rutas y poder exportarlas
 import { Router } from "express";
 //funcion de node js para leer archivos
-import {readFile} from 'fs/promises';
+import {readFile , writeFile} from 'fs/promises';
 
 //lee y trae el archivo
 const fileSells = await readFile('./data/ventas.json','utf-8')
@@ -22,7 +22,6 @@ router.post('/sells/', (req,res)=>{
 
       if(result){
 
-        // Crear un nuevo objeto que solo contenga las propiedades deseadas
         const respuesta = {
             numero_venta: result.id,
             fecha_venta: result.fecha,
@@ -30,6 +29,56 @@ router.post('/sells/', (req,res)=>{
         };
 
         res.status(200).json(respuesta)
+      }
+      else{
+         res.status(400).json('Venta no encontrada')
+      }
+ 
+      
+   } catch (error) {
+      res.status(500).json('Error en el servidor: ' + error.message);
+   }
+
+})
+
+router.delete('/deletesell/:id', async (req,res)=>{
+   try {
+
+      const id_venta = parseInt(req.params.id)
+
+        // Encontrar el índice del elemento a eliminar
+        const index = sellData.findIndex(e => e.id === id_venta);
+
+        if (index !== -1) {
+            // Elimina el elemento del array
+            sellData.splice(index, 1);
+
+            // Guardar los datos actualizados en el archivo
+            await writeFile('./data/ventas.json', JSON.stringify(sellData, null, 2), 'utf-8');
+
+            res.status(200).json({ message: "Venta eliminada con éxito" });
+        } else {
+            res.status(400).json('Venta no encontrada');
+        }
+ 
+      
+   } catch (error) {
+      res.status(500).json('Error en el servidor: ' + error.message);
+   }
+
+})
+
+
+router.get('/sells/:id', (req,res)=>{
+   try {
+
+      const id_venta = parseInt(req.params.id);
+
+      const result = sellData.find(e => e.id == id_venta)
+
+
+      if(result){
+         res.status(200).json(result)
       }
       else{
          res.status(400).json('Venta no encontrada')
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,7 @@
 //funcion de express para crear rutas y poder exportarlas
 import { Router } from "express";
 //funcion de node js para leer archivos
-import {readFile} from 'fs/promises';
+import {readFile, writeFile} from 'fs/promises';
 
 //lee y trae el archivo
 const fileUsers = await readFile('./data/usuarios.json','utf-8')
@@ -11,16 +11,72 @@ const userData = JSON.parse(fileUsers)
 
 const router = Router()
 
-router.post('/users/', (req,res)=>{
+router.get('/users/:id', (req,res)=>{
    try {
 
-      const id_usuario = req.body.id
+      const id_usuario = parseInt(req.params.id);
 
       const result = userData.find(e => e.id == id_usuario)
 
 
       if(result){
          res.status(200).json(result)
+      }
+      else{
+         res.status(400).json('Usuario no encontrado')
+      }
+ 
+      
+   } catch (error) {
+      res.status(500).json('Error en el servidor: ' + error.message);
+   }
+
+})
+
+router.post('/newuser/', async (req,res)=>{
+   try {
+
+      const lastUserId = userData[userData.length - 1].id;
+
+      const { nombre, apellido, email, contraseña } = req.body;
+
+        // Verificación de que todos los campos requeridos están presentes
+        if (!nombre || !apellido || !email || !contraseña) {
+            return res.status(400).json({ error: 'Todos los campos son obligatorios: nombre, apellido, email y contraseña' });
+        }
+
+
+       const new_user =   {
+         "id": lastUserId+1,
+         "nombre": nombre,
+         "apellido": apellido,
+         "email": email,
+         "contraseña": contraseña
+       }
+
+       userData.push(new_user);
+       await writeFile('./data/usuarios.json', JSON.stringify(userData, null, 2), 'utf-8');
+       res.status(200).json("Usuario registrado con exito")
+      
+   } catch (error) {
+      res.status(500).json('Error en el servidor: ' + error.message);
+   }
+
+})
+
+router.put('/email/', async (req,res)=>{
+   try {
+
+      const id_usuario = req.body.id
+      const email_nuevo = req.body.email
+
+      const result = userData.find(e => e.id == id_usuario)
+
+
+      if(result){
+         result.email = email_nuevo;
+         await writeFile('./data/usuarios.json', JSON.stringify(userData, null, 2), 'utf-8');
+         res.status(200).json(`El email del Sr.${result.nombre + " " + result.apellido} fue modificado correctamente`)
       }
       else{
          res.status(400).json('Usuario no encontrado')
